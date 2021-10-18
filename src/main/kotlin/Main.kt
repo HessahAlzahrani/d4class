bad141cd8927d9e60e5500de2dddeb9556142cb5
hessah <hessah.mohammed.ruh.k@tuwaiq.edu.sa>
2021-10-18 09:17:04 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+import jdk.dynalink.Operation
+import java.lang.ArithmeticException
+
+fun main(){
+    //--------------------------------------
+val  result= aaaa(10,30)
+println(result)
+    val result2= aaaa2(7,4)
+//----------------------oprator----------------------------------
+var peverage = readLine()!!.capitalize()
+    //   peverage = null
+    println (peverage)
+
+
+//-----------------Exception--------------------------------
+
+
+    try{
+        var num = 5/0
+        println("num")
+    }
+    catch (ex:ArithmeticException){
+        println(ex.message)
+if (ex.message=="/ by zero"){
+    println("لاتقسم على الصفر ")
+}
+        else{
+            println(ex.message)
+        }
+    }
+    finally {
+        println("true")
+    }
+//------------------------------------------------
+
+
+}
+
+val aaaa = fun (x: Int,Y:Int):Int{
+    return x+Y
+}
+//----------------------------------------
+val aaaa2 = fun (t:Int, a:Int)= t-a
+
+//--------------------------------------------------------
